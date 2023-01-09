236e358110da6da33bff3df3f9763c88eafb02a6
minin_maxim <mininmaxim87@gmail.com>
2023-01-09 20:55:23 +0300

    удалил класс А, который есть в отстающей ветке, добавил изменения в другие классы

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Я создаю первый коммин в ветке master");
+        System.out.println("Добавляю новый коммит в ветке 3, в мэйн классе");
     }
 }
