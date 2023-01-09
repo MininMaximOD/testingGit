a4b220701b4a5cc10b0ec0d8415709a3a32bf503
minin_maxim <mininmaxim87@gmail.com>
2023-01-09 20:42:50 +0300

    второй коммит в новой ветке 1, который не попадает в мастер

---
+++ src/ClassB.java
@@ -0,0 +1,5 @@
+public class ClassB {
+    public void method1(){
+        System.out.println("Создаю класс В, который попадает в коммит, но не попадает в мердж риквест, его нету в ветке мастер");
+    }
+}
