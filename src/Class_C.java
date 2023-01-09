62f08f95d880aa051d37e64c49fb497592f91de2
minin_maxim <mininmaxim87@gmail.com>
2023-01-09 20:51:44 +0300

    создал новый класс Д и добавил изменения в класс С

--- src/Class_C.java
+++ src/Class_C.java
@@ -2,5 +2,6 @@ public class Class_C {
 
     public void method(){
         System.out.println("Создаю класс С, который попадет в мастер, но его нету в отстающей ветке");
+        System.out.println("Добавляю изменения в класс С");
     }
 }
---
+++ src/Class_D.java
@@ -0,0 +1,5 @@
+public class Class_D {
+    public void method(){
+        System.out.println("создаю класс Д, который попадет в мастер, но не попадет в отстающую ветку");
+    }
+}
