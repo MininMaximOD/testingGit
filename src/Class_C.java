80cf3d60c00c2b65142811181ef1076a3feb0b2f
minin_maxim <mininmaxim87@gmail.com>
2023-01-09 21:23:22 +0300

    коммит для слияния после старой ветки

--- src/Class_C.java
+++ src/Class_C.java
@@ -1,7 +1,7 @@
 public class Class_C {
 
     public void method(){
-        System.out.println("Создаю класс С, который попадет в мастер, но его нету в отстающей ветке");
+        System.out.println("Создаю класс С, который попадет в мастер, но в старой ветке нету");
         System.out.println("Добавляю изменения в класс С");
         System.out.println("еще немного изменений");
     }
